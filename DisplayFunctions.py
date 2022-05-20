aad69f508c18aa6e4940c88674773243441b5ca7
NathanFortyTwo <94806287+NathanFortyTwo@users.noreply.github.com>
2022-05-20 20:48:40 +0200

    Update DisplayFunctions.py

--- DisplayFunctions.py
+++ DisplayFunctions.py
@@ -10,8 +10,13 @@ from skimage import exposure
 
 
 def imageHist(image):
-    # Génère et affiche l'histogramme d'une image (nuance de gris ou normal)
-    # image est un array
+    """
+    - image : np.array
+
+    Génère et affiche l'histogramme d'une image, qu'elle soit en nuance de gris ou en couleurs
+
+    Est appelé par : showGrey
+    """
 
     _, axis = plt.subplots(ncols=2, figsize=(12, 3))
     if (image.ndim == 2):
@@ -22,6 +27,7 @@ def imageHist(image):
         axis[0].set_title('Grayscale Image')
         hist = exposure.histogram(image)
         axis[1].plot(hist[0])
+
     else:
         # Color image
 
@@ -36,13 +42,17 @@ def imageHist(image):
 
 
 
-def showGrey(str,bol):
-    # Affiche l'histogramme d'une image et si bol = True, d'une image filtrée
-    # bol est un boolean qui permet d'afficher ou non l'histogramme de l'image filtrée
+def showGrey(path,boolean):
+    """
+    - path : String, contient le chemin d'accès vers une image
+    - boolean : Bool, permet d'afficher ou non l'histogramme de l'image filtrée
 
-    image = imread(str, as_gray=True)
+
+
+"""
+    image = imread(path, as_gray=True)
     imageHist(image)
-    if bol:
+    if boolean:
         imageHist(pc.convuProduct(image))
     plt.show()
 
